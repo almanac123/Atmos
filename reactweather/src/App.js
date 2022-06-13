da4ccbb42e594e8ef1035460b19333e3792a866c
Anubhab Patra <96374627+almanac123@users.noreply.github.com>
2022-06-13 17:15:30 +0530

    Add files via upload

---
+++ reactweather/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weathercard from './components/weather/weathercard';
+import Temp from './components/weather/temp';
+
+const App=()=> {
+  return (
+    <>
+    <Temp/>
+    </>
+  )
+}
+
+export default App;
